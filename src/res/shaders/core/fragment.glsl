6f5c075eb91397d72dd93be22ba93bdc3b53be14
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-01-05 17:03:05 -0500

    ok so making sure I have a home base to return too with this commit

--- src/res/shaders/core/fragment.glsl
+++ src/res/shaders/core/fragment.glsl
@@ -4,13 +4,13 @@ in vec3 out_color;
 vec2 out_texture_coords;
 
 // Uniforms:
-uniform sampler2D texture1;
+// uniform sampler2D texture1;
 // uniform sampler2D texture2;
 
 // Output:
 out vec4 fragColor;
 
 void main(void) {
-    // fragColor = vec4(out_color, 1.0);
-    fragColor = texture(texture1, out_texture_coords);
+    fragColor = vec4(out_color, 1.0);
+    // fragColor = texture(texture1, out_texture_coords);
 }
